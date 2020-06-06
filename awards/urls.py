7559ee12c1fba3c20aed21ace0e26a65e54a7f78
ow-tech <wagwanwekon@outloo.com>
2020-06-06 15:13:23 +0300

    creation of login path and template

--- awards/urls.py
+++ awards/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from  . import views as awards_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', awards_views.home, name= 'home'),
-    path('accounts/register', awards_views.register, name='registration')
+    path('accounts/register', awards_views.register, name='register'),
+    path('accounts/login', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- myawards/settings.py
+++ myawards/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static"),
 ]
+
+CRISPY_TEMPLATE_PACK ='bootstrap4'
