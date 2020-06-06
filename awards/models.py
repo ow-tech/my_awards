31bb9b3f5c5bef852e25e5a4c5f6494edb09e527
ow-tech <wagwanwekon@outloo.com>
2020-06-06 13:09:05 +0300

    creation of project model and; save and delete methods and their tests

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    project_name = models.CharField(max_length=30)
+    project_description = models.TextField()
+    project_image = models.ImageField(upload_to='images/', default='some string')
+    project_live_link =models.URLField(max_length=100000, default='some string')
+
+
+
+    def __str__(self):
+        return self.author, self.project_name
+
+ 
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def delete_project(cls, id):
+        project = cls.objects.filter(id).all()
+        project.delete()
--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.new_project= Project(project_name='Delani', project_description="A website for delani photo studio")
+
+    def test_instance(self):
+        self.assertTrue( isinstance(self.new_project, Project))
+
+    def test_save_method(self):
+        self.new_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) >0)
+
+    def test_delete_method(self, id):
+        self.new_project.save_project()
+        self.new_project.delete_project(id)
+        after = Project.object.all()
+        self.assertTrue(len(after)< 1)
