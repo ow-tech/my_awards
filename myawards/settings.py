07df0ae2c6c52ade837e95c986a850308674ed04
ow-tech <wagwanwekon@outloo.com>
2020-06-06 10:55:41 +0300

    created database

--- myawards/settings.py
+++ myawards/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'myawards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        # 'HOST': os.environ.get('DB_HOST'),
+        # 'PORT': os.environ.get('DB_PORT'),
+        'TEST': {
+            'NAME': 'test_instagram'
+        }
+    },
 }
 
 
@@ -107,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
