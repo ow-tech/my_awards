012599de84d7692e70a0237b4c4fbd745986f7c9
ow-tech <wagwanwekon@outloo.com>
2020-06-09 22:56:52 +0300

    changing db formart in settings

--- myawards/settings.py
+++ myawards/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        # 'HOST': os.environ.get('DB_HOST'),
-        # 'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'TEST': {
             'NAME': 'test_awards'
         }
